a2046dd70b0e7ef903db01ee93c155bb733ad5e5
Quin Scacheri <qs366@nyu.edu>
2020-01-09 17:39:00 -0500

    mobile version and other stuff

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 $(document).ready(function(){
- $("#name").hide().delay(500).fadeIn(1000);
+    $("#name").hide().delay(500).fadeIn(1000);
+    if (window.scrollY > window.innerHeight)
+    {
+        console.log("passed");
+        $("#back").show();
+    }
+    else {
+        $("#back").hide();
+    }
+});
+
+$(window).scroll(function () {
+    // console.log(window.scrollY);
+    if (window.scrollY > window.innerHeight)
+    {
+        console.log("passed");
+        $("#back").show();
+    }
+    else {
+        $("#back").hide();
+    }
 });
