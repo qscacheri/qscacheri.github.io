1c8425deab5e92996566523aaa99f37a340dc77c
Quin Scacheri <33070018+qscacheri@users.noreply.github.com>
2020-05-20 16:49:27 -0400

    added parallax css

--- quinscacheri/src/js/App.jsx
+++ quinscacheri/src/js/App.jsx
@@ -5,20 +5,16 @@ import { Parallax, Background } from 'react-parallax';
 import Resume from './Resume'
 function App() {
   return (
-      <div className="App">
-         <Parallax
-            blur={10}
-            bgImage={require('../images/book.jpg')}
-            bgImageAlt="the cat"
-            strength={200}
-        >
-            Put some text content here - even an empty div with fixed dimensions to have a height
-            for the parallax.
-            <div style={{ height: '200px' }} />
-            <div style={{ height: '200px' }} />
+    <div className="App">
+      <div className="parallax-wrapper">
 
-        </Parallax>
+      <Portfolio />
+
       </div>
+
+   
+
+    </div>
 
   );
 }
